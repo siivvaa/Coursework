0fc25c1ae5124382f77349393d7f2f03b91f7efe
siivvaa <82778796+siivvaa@users.noreply.github.com>
2022-03-24 13:34:08 +0530

    Update bestFit.cpp

--- OS Lab/Lab - 8/bestFit.cpp
+++ OS Lab/Lab - 8/bestFit.cpp
@@ -1,49 +1,65 @@
-#include<iostream>
- 
+#include<bits/stdc++.h>
 using namespace std;
- 
+
+// Function to allocate memory to blocks as per Best fit
+// algorithm
+void bestFit(int blockSize[], int m, int processSize[], int n)
+{
+    // Stores block id of the block allocated to a
+    // process
+    int allocation[n];
+
+    // Initially no block is assigned to any process
+    memset(allocation, -1, sizeof(allocation));
+
+    // pick each process and find suitable blocks
+    // according to its size ad assign to it
+    for (int i=0; i<n; i++)
+    {
+        // Find the best fit block for current process
+        int bestIdx = -1;
+        for (int j=0; j<m; j++)
+        {
+            if (blockSize[j] >= processSize[i])
+            {
+                if (bestIdx == -1)
+                    bestIdx = j;
+                else if (blockSize[bestIdx] > blockSize[j])
+                    bestIdx = j;
+            }
+        }
+
+        // If we could find a block for current process
+        if (bestIdx != -1)
+        {
+            // allocate block j to p[i] process
+            allocation[i] = bestIdx;
+
+            // Reduce available memory in this block.
+            blockSize[bestIdx] -= processSize[i];
+        }
+    }
+
+    cout << "\nProcess No.\tProcess Size\tBlock no.\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << "   " << i+1 << "\t\t" << processSize[i] << "\t\t";
+        if (allocation[i] != -1)
+            cout << allocation[i] + 1;
+        else
+            cout << "Not Allocated";
+        cout << endl;
+    }
+}
+
+// Driver code
 int main()
 {
-int fragment[20],b[20],p[20],i,j,nb,np,temp,lowest=9999;
-static int barray[20],parray[20];
+    int blockSize[] = {100, 500, 200, 300, 600};
+    int processSize[] = {212, 417, 112, 426};
+    int m = sizeof(blockSize)/sizeof(blockSize[0]);
+    int n = sizeof(processSize)/sizeof(processSize[0]);
 
-cout<<"\nEnter the number of blocks:";
-cin>>nb;
-cout<<"Enter the number of processes:";
-cin>>np;
-cout<<"\nEnter the size of the blocks:-\n";
-for(i=1;i<=nb;i++)
-    {
-        cout<<"Block no."<<i<<":";
-        cin>>b[i];
-    }
-cout<<"\nEnter the size of the processes :-\n";
-for(i=1;i<=np;i++)
-    {
-        cout<<"Process no. "<<i<<":";
-        cin>>p[i];
-    }
-for(i=1;i<=np;i++)
-{
-for(j=1;j<=nb;j++)
-{
-if(barray[j]!=1)
-{
-temp=b[j]-p[i];
-if(temp>=0)
-if(lowest>temp)
-{
-parray[i]=j;
-lowest=temp;
-}
-}
-}
-fragment[i]=lowest;
-barray[parray[i]]=1;
-lowest=10000;
-}
-cout<<"\nProcess_no\tProcess_size\tBlock_no\tBlock_size\tFragment";
-for(i=1;i<=np && parray[i]!=0;i++)
-cout<<"\n"<<i<<"\t\t"<<p[i]<<"\t\t"<<parray[i]<<"\t\t"<<b[parray[i]]<<"\t\t"<<fragment[i];
-return 0;
-}
+    bestFit(blockSize, m, processSize, n);
+
+    return 0 ;
